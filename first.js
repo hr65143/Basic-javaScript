ff8964d70de57baac14143bd116444958db76db8
hr65143 <hamirraza24@gmail.in>
2026-06-23 23:28:13 +0530

    complebasicScript

--- first.js
+++ first.js
@@ -1,2 +1,84 @@
-console.log("Hello HR");
+// console.log("Hello Rush welcome to my world");
 // in a console i want print this line code
+// console.log("i'm HR");
+// name="HR";
+// age=24;
+// price = 99.99;
+// x = null;
+// we know something value is empty
+// null nothing in x 
+// y =undefined;
+// isFollow = false;
+// console.log(name);
+// console.log(age);
+// console.log(price);
+// console.log(x);
+// console.log(y);
+// console.log(isFollow);
+// name=65;
+// danmically typed language it's not like varible fixed for data type
+// console.log(name);
+
+// java Script is case sensative 
+
+// fullName = "HR";
+// FULLNAME ="Rus";
+// _name="HR";
+// $name="rs";
+
+// console.log(FullName);
+// console.log(FULLNAME);
+
+//  varible decleration
+// let fullName = "HR";
+// let age =24;
+let totalPrice=650;
+// console.log(name);
+// we can't use var now days var can redeclare again
+// var age=65;
+// var age=90;
+// var age=56;
+// console.log(age);
+// most we perfer let for declaring variable we can't redeclare like var
+// we can update let
+
+
+// let age=24;
+// age=23;
+// console.log(age);
+// nither redeclare nor update is const
+// const PI =3.14;
+// console.log(PI);
+// let is block scope
+
+
+// {
+//     let a=5;
+//     console.log(a);
+// }
+// {
+//     let a=10;
+//     console.log(a);
+// }
+
+// data type in java Script premitive (7)and non primetive(ex obj)
+// let age =24;
+// console.log(age);
+// let price =100.0;
+// let fullName ="HR";
+//  let isFollow =true;
+//  let x=null;
+//  let x1=BigInt("14364");
+
+//  Non-primitive :object-collection of values
+const student = {
+    fullName:"HR",
+    age:24,
+    cgpa:7.1,
+    isPass:true,
+
+};
+// console.log(student["age"]);
+// console.log(student.age);
+student["age"]=student["age"]+1;
+console.log(student["age"]);
