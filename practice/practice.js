456871a6bb19e92ed48c8438ac3969800e9ebf5c
hr65143 <hamirraza24@gmail.in>
2026-06-23 23:53:04 +0530

    practice

---
+++ practice/practice.js
@@ -0,0 +1,21 @@
+const product ={
+    title:"parker Jotter Standard CT Ball pen (Black)",
+    rating:4,
+    offer:5,
+    isDeal:true,
+    price:270,
+
+};
+console.log(product);
+
+const profile ={
+    username:"hn121",
+    following:140,
+    follower:340,
+    post:5,
+    isFollow:true,
+    
+};
+console.log(profile);
+console.log(typeof profile);
+console.log(typeof profile["username"]);
