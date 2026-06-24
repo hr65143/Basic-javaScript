32429a12bf4a8be3bc26275aa03d52fd1167336a
hr65143 <hamirraza24@gmail.in>
2026-06-24 11:43:05 +0530

    basicOperators

---
+++ operator&condional-statement/operator.js
@@ -0,0 +1,71 @@
+// console.log("Hello world");
+// Arthmetic operators
+// let a=5;
+// let b=2;
+// let c = a+b ;
+// console.log("a =",a," b =",b)
+// console.log("a+b =", a+b);
+// console.log("a -b =", a-b);
+// console.log("a*b =", a*b);
+// console.log("a/b =", a/b);
+// console.log("a%b =", a%b);
+// console.log("a**b = ", a**b);
+
+// unary operator
+// a++;
+// console.log("a =", a);
+// a--;
+// console.log("a =", a);
+
+// pre Increment first incrase then print
+// console.log("++a =", ++a);
+// post Increment orginal value print then increase
+// console.log("a++ =", a++);
+
+
+
+// Asigment operators
+// let a = 5;
+// let b = 2;
+// a += 4; // a = a+4
+// console.log("a =", a);
+// b -= 1; // b =b-1
+// console.log("b =", b);
+// a *= 4;
+// console.log("a =", a);
+// a /= 4;
+// console.log("a =",a);
+
+// a %= 4;
+// console.log("a =",a);
+
+
+// comparisoin operators
+
+// let a = 5;
+// let b = 2; 
+// let c = "5";
+
+// console.log("a == b", a == b);
+// console.log(" a != b", a!=b);
+// console.log("a == c", a == c);
+// console.log("a === c", a === c);
+// console.log("a !==b", a !==b);
+// console.log("a > b", a > b);
+// console.log("a <= c", a<=c);
+
+
+// logical operators
+
+let a = 6;
+let b = 5;
+
+ let cond1 = a > b;  // true
+let cond2 =  a == 6; // true
+console.log("cond1 && cond2 =", cond1 && cond2);// both condition must ture
+console.log("cond1 || cond2 =",a<b || a ===6 );// either one must ture
+console.log("!(6<5) =", !(a<b));
+
+
+
+
